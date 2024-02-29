61e7c6b601cadb264f6c01eefef3cca30949114a
DavidFerX2 <dvdferx2@gmail.com>
2024-02-29 01:14:29 +0100

    feat: add .gitignore

--- run.bash
+++ run.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Compile both C files and link them
-gcc main.c nn.c -o main
+gcc main.c nn.c -o ./out/main
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
     # Run the executable
-    ./main
+    ./out/main
 else
     echo "Compilation failed."
 fi
