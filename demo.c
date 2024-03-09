b9d970f1d9a9721525cec17cb01e32e776e97dea
DavidFernandezUD <david.f@opendeusto.es>
2024-03-09 17:35:25 +0100

    add NN demo

--- demo.c
+++ demo.c
@@ -2,46 +2,52 @@
 #include <time.h>
 #include "src/blas/blas.h"
 #include "src/layer.h"
+#include "src/nn.h"
 
 
 int main() {
-    
+
     srand(time(NULL));
+
+    // <========= XOR MODEL =========>
+    Layer layers[3] = {
+        layer_alloc(2, 4, RELU),
+        layer_alloc(4, 4, RELU),
+        layer_alloc(4, 1, SIGMOID)
+    };
+
+    NN nn = nn_alloc(3, layers);
+
+    nn_print(nn);
+
+    float data[] = {
+        0, 0,
+        1, 0,
+        0, 1,
+        1, 1
+    };
+    Matrix X = mat(4, 2, data);
+
+    float data2[] = {
+        0,
+        1,
+        1,
+        0
+    };
+    Matrix y = mat(4, 1, data2);
+
+    nn_forward(&nn, X);
+    printf("===== Outputs =====\n\n");
+    mat_print(nn.outputs);
+
+    // <========= TRAINING =========> 
+    printf("\n\n===== TRAINING =====\n\n");
+    nn_fit(&nn, X, y, 50000, 0.01f);
+
+    nn_forward(&nn, X);
+    printf("===== Outputs =====\n\n");
+    mat_print(nn.outputs);
     
-    Matrix inputs = mat_alloc(10, 16);
-    mat_rand(inputs, -10, 10);
-
-    Layer layer1 = layer_alloc(16, 128, LEAKY_RELU);
-    Layer layer2 = layer_alloc(128, 128, LEAKY_RELU);
-    Layer layer3 = layer_alloc(128, 4, IDENTITY);
-
-    // Prediction
-    printf("Inputs: \n");
-    mat_print(inputs);
-    printf("-------------------------------------------\n");
-    printf("Outputs: \n");
-    
-    layer_forward(&layer1, inputs);
-    layer_forward(&layer2, layer1.outputs);
-    layer_forward(&layer3, layer2.outputs);
-
-    mat_print(layer3.outputs);
-
-    // Prediction 2
-    printf("\n=============== PREDICTION 2 ==============\n");
-    Matrix input_slice = mat_row_slice(inputs, 0, 5);
-
-    printf("Inputs: \n");
-    mat_print(input_slice);
-    printf("-------------------------------------------\n");
-    printf("Outputs: \n");
-    
-    layer_forward(&layer1, input_slice);
-    layer_forward(&layer2, layer1.outputs);
-    layer_forward(&layer3, layer2.outputs);
-
-    mat_print(layer3.outputs);
-
     return 0;
 }
 
